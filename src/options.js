b8df53f4dbadfa1bd189e3da95dbd959ee63e06c
bwrobinett <bwrobinett@f4a40a62-e9bc-11de-9c2a-cb0f5ba72f9d>
2010-04-26 07:28:23 +0000

    Working on options page

---
+++ src/options.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2010 Brent Weston Robinett <bwrobinett@gmail.com>
+ * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
+ */ 
+(function () {
+	var background = chrome.extension.getBackgroundPage();	
+	var cCalc = background.cCalc;
+	var optionSel = "input[type=text]";
+	var $options = $(optionSel);
+	function loadOptionValues() {
+		console.debug("otpoins", $options.length);
+		$(optionSel).each(function() {
+			var val = JSON.parse(localStorage["opt_"+this.id] || "[]")[0];
+			if (this.id !== "height") {
+				this.value = val || "";
+			} else {
+				this.value = val || "";
+			}
+		})
+	}
+	
+	loadOptionValues();
+	
+	function updateOption() {
+		cCalc.calcCmd[this.id](this.value);
+		return this;
+	}
+	$(document).delegate(optionSel, "keyup change blur", updateOption);
+	$(document).delegate(".reset", "click", function () {		
+		var $option = $(this).closest("tr").find("input").val("").focus();
+		updateOption.call($option[0].id);
+	});	
+}());
